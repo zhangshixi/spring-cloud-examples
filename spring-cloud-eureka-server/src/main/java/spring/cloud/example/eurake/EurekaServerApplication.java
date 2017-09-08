3631606026dedb94674a4ba0d4ea17bf0f33eb25
zhangshixi <shixizhang@foxmail.com>
2017-09-09 00:05:28 +0800

    spring-cloud-eureka-server initial commit

---
+++ spring-cloud-eureka-server/src/main/java/spring/cloud/example/eurake/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package spring.cloud.example.eurake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Michael.Zhang
+ * @date 2016-11-13 23:10:06
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
