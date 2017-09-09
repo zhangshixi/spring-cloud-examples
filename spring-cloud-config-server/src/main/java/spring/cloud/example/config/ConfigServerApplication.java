aa9c902e1df74b22112998842244dd374e9e8cd1
zhangshixi <shixizhang@foxmail.com>
2017-09-09 22:54:24 +0800

    spring-cloud-config-server initial commit

---
+++ spring-cloud-config-server/src/main/java/spring/cloud/example/config/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package spring.cloud.example.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created 2016-11-13 23:16:53
+ *
+ * @author Michael.Zhang
+ */
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
