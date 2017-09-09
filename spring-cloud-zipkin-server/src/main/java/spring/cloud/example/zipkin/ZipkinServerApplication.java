930c880cd9a74bc840158abd91df9478b7b0897e
zhangshixi <shixizhang@foxmail.com>
2017-09-09 22:57:28 +0800

    spring-cloud-zipkin-server initial commit

---
+++ spring-cloud-zipkin-server/src/main/java/spring/cloud/example/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,22 @@
+package spring.cloud.example.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
+
+/**
+ * Created 2016-11-13 23:10:06
+ *
+ * @author Michael.Zhang
+ */
+@EnableEurekaClient
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+
+}
